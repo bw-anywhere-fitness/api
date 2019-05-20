bfd4af7bb3523a6cc828f8121c331c5eaaf64a56
deanovision <rodeanfraser@gmail.com>
2019-05-20 13:51:34 -0400

    fixed error on line 2 of indexjs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server/server.js");
-const port = process.env_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log("\n==RUNNING ON 5000==\n");
